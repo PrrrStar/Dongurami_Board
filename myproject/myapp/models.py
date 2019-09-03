3841607a22238aa9e21e5d613dd20f30eb7df9f9
쥐미니\LG <jmeef0802@gmail.com>
2019-09-04 01:37:44 +0900

    db 수정, 게시판 style 수정

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -7,8 +7,8 @@ from django.db import models
 class myDB(models.Model):
     title = models.CharField(max_length=400)
     tag = models.CharField(max_length=50)
-    writer = models.CharField(max_length=120)
-    contents = models.TextField()
+    writer = models.CharField(max_length=120, null=True, default='')
+    contents = models.TextField(null=True, default='')
     date = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
