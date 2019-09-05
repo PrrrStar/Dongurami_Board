a857e82493e31180a8bc61679000161633ae8df9
쥐미니\LG <jmeef0802@gmail.com>
2019-09-05 14:42:29 +0900

    AWS Server Hosting

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j#9890n)=2&&inx7b)i&p=^w7=ft#q+1j2x#edy0c6=@y4@3=)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://3.130.236.107']
 
 
 # Application definition
