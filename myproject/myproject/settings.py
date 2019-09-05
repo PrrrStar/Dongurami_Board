fa062ad7f2e27c7147f95cb1f6413f35be6cbba9
쥐미니\LG <jmeef0802@gmail.com>
2019-09-05 14:55:51 +0900

    no message

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j#9890n)=2&&inx7b)i&p=^w7=ft#q+1j2x#edy0c6=@y4@3=)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['http://3.130.236.107']
+ALLOWED_HOSTS = ['3.130.236.107',
+                '127.0.0.1']
 
 
 # Application definition
